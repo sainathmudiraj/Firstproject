ec064d38e049da9288cfb910a488cf86868f4042
sainathmudiraj <sainath2011mudiraj@gmail.com>
2021-08-18 14:51:14 +0000

    first commit

---
+++ src/main/java/com/sainath/app/App.java
@@ -0,0 +1,13 @@
+package com.sainath.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
